b0d5f8948f9c8df92f8a2c075eccc6bd94a564a8
JohnMcCarroll <jtm5356@rit.edu>
2026-03-31 16:01:35 -0400

    cdk stack: ec2, bucket, glue role

--- financial-assistant-cdk/financial_assistant_cdk/financial_assistant_cdk_stack.py
+++ financial-assistant-cdk/financial_assistant_cdk/financial_assistant_cdk_stack.py
@@ -1,7 +1,9 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_s3 as s3,
+    aws_ec2 as ec2,
+    aws_iam as iam,
+    RemovalPolicy
 )
 from constructs import Construct
 
@@ -10,10 +12,33 @@ class FinancialAssistantCdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # 1. Create VPC for ChromaDB instance
+        self.vpc = ec2.Vpc(self, "AssistantVPC",
+            max_azs=2,
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                name="Public",
+                subnet_type=ec2.SubnetType.PUBLIC,)
+            ]
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "FinancialAssistantCdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # 2. Create the S3 Data Lake
+        self.bucket = s3.Bucket(self, "FinancialDataLake",
+            versioned=True,
+            removal_policy=RemovalPolicy.DESTROY,
+            auto_delete_objects=True
+        )
+
+        # 3. Create a Role for AWS Glue
+        self.glue_role = iam.Role(self, "GlueIngestionRole",
+            assumed_by=iam.ServicePrincipal("glue.amazonaws.com")
+        )
+        # Give role access to S3 and Bedrock
+        self.bucket.grant_read_write(self.glue_role)
+        self.glue_role.add_to_policy(iam.PolicyStatement(
+            actions=["bedrock:InvokeModel"],
+            resources=["*"] #TODO: specific model scope
+        ))
+
+        # Output the Bucket Name to your terminal after deployment
+        self.export_value(self.bucket.bucket_name, name="DataLakeName")
